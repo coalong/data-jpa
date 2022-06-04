9e7413c468215413b8f52bb3b1fc470780ef6276
coalong <minay0904@gmail.com>
2022-06-04 22:30:34 +0900

    + 자바 JPA 페이징 처리

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -29,7 +29,13 @@ public class Member {
     public Member(String userName, int age, Team team) {
         this.userName = userName;
         this.age = age;
+        if (team != null) {
         changeTeam(team);
+        }
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
     }
 
     public void changeTeam(Team team) {
